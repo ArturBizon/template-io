a6382f60642e5b17476a5895bc9db77e40aec77c
ArturBizon <artur_bizon@hotmail.com>
2026-02-18 09:52:12 -0300

    Projeto Inicial

---
+++ src/main/java/br/furb/aula/poo/primeiraAtividade/App.java
@@ -0,0 +1,15 @@
+package br.furb.aula.poo.primeiraAtividade;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Ola ");
+    }
+}
